c1a812529b48450c7de100483d542e84191acd3f
Randy Taylor <randyisbusy@gmail.com>
2019-07-23 19:42:34 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'swap_elements_from_to'do
   it 'should return an array sorted by the index from location into the argument to index location.' do
-    expect(swap_elements_from_to([1,2,10])).to eq([10,2,1])
+    expect(swap_elements_from_to([1,2,10], 2, 0)).to eq([10,2,1])
 end
 end
 describe 'collections practice' do
